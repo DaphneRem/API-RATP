266b587e5dc05994e4802dcc7879c552b3f03d75
daphne remali <remalidaphne@gmail.com>
2017-02-25 15:43:29 +0100

    add new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import Input from './components/input/Input';
+import ButtonChaveaux from './components/buttonChaveaux/ButtonChaveaux';
+import ButtonRobespierre from './components/buttonRobespierre/ButtonRobespierre';
+import ButtonVincennes from './components/buttonVincennes/ButtonVincennes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Input/>
+          <ButtonChaveaux/>
+          <ButtonRobespierre/>
+          <ButtonVincennes/>
       </div>
     );
   }
